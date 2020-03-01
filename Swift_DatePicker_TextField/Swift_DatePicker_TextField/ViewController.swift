42042980f860926046ef362808b1b2375d968d2a
Isami Odagiri <gurentosouen@gmail.com>
2020-03-01 12:42:54 +0900

    プロジェクト作成

---
+++ Swift_DatePicker_TextField/Swift_DatePicker_TextField/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift_DatePicker_TextField
+//
+//  Created by Isami Odagiri on 2020/03/01.
+//  Copyright © 2020 Isami Odagiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift_DatePicker_TextField/Swift_DatePicker_TextFieldTests/Swift_DatePicker_TextFieldTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift_DatePicker_TextFieldTests.swift
+//  Swift_DatePicker_TextFieldTests
+//
+//  Created by Isami Odagiri on 2020/03/01.
+//  Copyright © 2020 Isami Odagiri. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_DatePicker_TextField
+
+class Swift_DatePicker_TextFieldTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
